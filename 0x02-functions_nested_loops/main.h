20c612ae98c95ebdcaa48bfbf1b0526d8c368035
semudia <ekaniyere2002@gmail.com>
2022-10-20 07:29:19 -0700

    task 100

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,5 +15,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
+void print_times_table(int n);
 
-#endif
+#endif /* MAIN_H */
