91b81b5ac782ea6f8f7d0762c5f4d7a14111d703
semudia <ekaniyere2002@gmail.com>
2022-10-20 03:03:49 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -function to print abc 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char letter;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
